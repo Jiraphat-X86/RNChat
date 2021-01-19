ea68ee7185a0aca52743cb29d71e4c297aedd2b1
Jiraphat-X86 <b5908042@gmail.com>
2021-01-19 23:29:45 +0700

    update chat

--- chat/App.js
+++ chat/App.js
@@ -1,114 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base'
+import { StyleSheet, ScrollView,  View, Text, StatusBar } from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const init_state = {
+  name: 'Jiraphat-X86 official store',
+  messages: [
+    {
+      _id: 'sjnsdjnsdfndf,mdnf,',
+      text: 'How are you?',
+      createdAt: new Date(),
+      user: {
+          _id: 'fsnsllNSKksdfjds;KJ',
+          name: 'Jiraphat-X86',
+          avatar: 'https://placeimg.com/140/140/any',
+      }
+    },
+    {
+      _id: 'sdfsdmfn,dfmn,dfmm,m,ss',
+      text: 'Hello developer',
+      createdAt: new Date(),
+      user: {
+          _id: 'zlm;lfm',
+          name: 'Jiraphat-X86 official store',
+          avatar: 'https://placeimg.com/140/140/any',
+      }
+    },
+    {
+      _id: 'sdfkmsdf,sdmf,s',
+      text: 'Hello admin',
+      createdAt: new Date(),
+      user: {
+          _id: 'fsnsllNSKksdfjds;KJ',
+          name: 'Jiraphat-X86',
+          avatar: 'https://placeimg.com/140/140/any',
+      }
+    }
+  ]
+}
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state=init_state
+}
+  render() {
+    const { name, messages } = this.state
+    return (
+      <View style={{flex: 1, backgroundColor: 'whitesoke'}}>
+        <Header>
+          <Body>
+            <Title>{name}</Title>
+          </Body>
+        </Header>
+        <GiftedChat
+          messages={messages}
+          placeholder='พิมพ์ข้อความ...'
+          onSend={messages => this.onSendMessage(messages)}
+          user={{ _id: 'fsnsllNSKksdfjds;KJ' }}
+        />
+      </View>
+    )
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  onSendMessage = (value) => {
+    console.log(value)
+  }
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+const styles = StyleSheet.create({})
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default App
